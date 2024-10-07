25cf7f708f93c1ad334f40ddbca0b6d431471d41
Selina <57713712+SelinaDev@users.noreply.github.com>
2024-10-07 13:17:09 +0200

    Changed the mouseover checker to realtime checking
    fixes #28

--- part_13/src/GUI/mouseover_checker.gd
+++ part_13/src/GUI/mouseover_checker.gd
@@ -2,20 +2,14 @@ extends Node2D
 
 signal entities_focussed(entity_list)
 
-
-var _mouse_tile := Vector2i(-1, -1)
-
 @onready var map: Map = get_parent()
 
 
-func _unhandled_input(event: InputEvent) -> void:
-	if event is InputEventMouseMotion:
-		var mouse_position: Vector2 = get_local_mouse_position()
-		var tile_position: Vector2i = Grid.world_to_grid(mouse_position)
-		if _mouse_tile != tile_position:
-			_mouse_tile = tile_position
-			var entity_names = get_names_at_location(tile_position)
-			entities_focussed.emit(entity_names)
+func _process(_delta: float) -> void:
+	var mouse_position: Vector2 = get_local_mouse_position()
+	var tile_position: Vector2i = Grid.world_to_grid(mouse_position)
+	var entity_names = get_names_at_location(tile_position)
+	entities_focussed.emit(entity_names)
 
 
 func get_names_at_location(grid_position: Vector2i) -> String:
